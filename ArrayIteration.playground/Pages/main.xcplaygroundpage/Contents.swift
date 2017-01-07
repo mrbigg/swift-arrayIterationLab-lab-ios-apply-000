10a7d855825db3ce03599500c9d6b0fda851df80
Barry Levy <barry.levy@gmail.com>
2017-01-07 17:07:28 -0500

    Complete problems

--- ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -16,7 +16,7 @@
 // write your code here
 
 
-
+let cart:[String] = ["Chips","Salsa", "Guacamole", "Red wine"]
 
 
 
@@ -25,10 +25,7 @@
  */
 // write your code here
 
-
-
-
-
+let numbers:[Int] = [1,2,3,4,5,6,7,8,9,10]
 
 
 /*: question3
@@ -36,21 +33,18 @@
  */
 // write your code here
 
-
-
-
-
-
+for number in numbers {
+print(number)
+}
 
 /*: question4
  ### 4. Take the list of shopping cart items you created in question 1 and print their values in the most efficient way possible. Prefix each item in the array with a string of text.
  */
 // write your code here
 
-
-
-
-
+for item in cart {
+    print("String example " + item)
+}
 
 
 /*: question5
@@ -59,17 +53,26 @@
 // write your code here
 
 
+func printItem(item:String) {
+    print(item)
+}
 
-
-
-
-
+for c in cart {
+    printItem(item: c)
+}
 /*: question6
  ### 4. Create a function that takes an array of names and greets each person with the following message "Good morning x" <- x being the individuals name. One caveat. If the persons first name is Michael, we want to greet them with the message, "Top of the morning Michael!".
  */
 // write your code here
 
+let names = ["Michael","Barry", "Amy"]
 
+for name in names {
+    print("Good morning \(name)")
+    if name == "Michael" {
+        print ("Top of the morning, \(name)")
+    }
+    }
 
 
 
@@ -79,10 +82,16 @@
  */
 // write your code here
 
+let array = [40, 60, 50, 52, 59, 13, 90, 100, 5, 52, 51, 49]
 
+func integersLessThan50(ints:[Int]) -> [Int] {
+    var intsLessThan50: [Int] = []
+    for int in ints {
+        if int < 50 {
+            intsLessThan50.append(int)
+        }
+    }
+    return intsLessThan50
+}
 
-
-
-
-
-
+print(integersLessThan50(ints: array))
